651f367af8e07830d982ffcd0cdf593d4b2e4069
laura <laura.apremont@hotmail.fr>
2022-03-22 13:06:41 +0100

    update browser lunch

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,6 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -38,7 +37,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browser: ["ChromeHeadlessCI, Firefox"],
+    browsers: ['ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
